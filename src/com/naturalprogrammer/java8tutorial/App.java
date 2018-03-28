3fe9a0a3d6f70be6b471500fb94607e1db849cff
Marlon <martianworm17@gmail.com>
2018-03-28 17:00:11 +0800

    Passing Behavior

---
+++ src/com/naturalprogrammer/java8tutorial/App.java
@@ -0,0 +1,20 @@
+package com.naturalprogrammer.java8tutorial;
+
+/**
+ * 
+ * @author Lon
+ *
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(process("Hello World!"));
+	}
+
+	private static String process(String string) {
+		// TODO Auto-generated method stub
+		return string.toUpperCase();
+	}
+
+}
