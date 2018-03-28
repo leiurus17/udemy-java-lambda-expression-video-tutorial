557a645b031af0d7da252e0f3c88e4399cc001e0
Marlon <martianworm17@gmail.com>
2018-03-28 20:47:27 +0800

    Method References In Practice

--- src/com/naturalprogrammer/java8tutorial/App2.java
+++ src/com/naturalprogrammer/java8tutorial/App2.java
@@ -3,15 +3,19 @@ package com.naturalprogrammer.java8tutorial;
 import java.util.function.BiFunction;
 
 public class App2 {
-public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+
+		String prefix = "MR. ";
+
 		System.out.println(process("Hello World!", str -> str.toLowerCase()));
 		System.out.println(process("Hello World!", String::toLowerCase));
-		
+
 		System.out.println(process2("Hello World!", 5, (str, i) -> str.substring(i)));
 		System.out.println(process2("Hello World!", 5, String::substring));
 		
-		
+		System.out.println(process("Hello World!", str -> prefix.concat(str)));
+		System.out.println(process("Hello World!", prefix::concat));
+
 	}
 
 	private static String process2(String string, int i, BiFunction<String, Integer, String> processor) {
