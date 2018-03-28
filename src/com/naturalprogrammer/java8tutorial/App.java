075e9adf7e5624171db91f29f7481f7f6727f978
Marlon <martianworm17@gmail.com>
2018-03-28 18:25:27 +0800

    Lambda Expression

--- src/com/naturalprogrammer/java8tutorial/App.java
+++ src/com/naturalprogrammer/java8tutorial/App.java
@@ -9,12 +9,9 @@ public class App {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(process("Hello World!", new Processor () {
-			
-			public String process(String str) {
+		System.out.println(process("Hello World!", (String str) -> {
 				return str.toLowerCase();
-			}
-		}));
+			}));
 	}
 
 	private static String process(String string, Processor processor) {
