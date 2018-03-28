94e4b5318a16516d4a6aabdc7f8ca12f61619924
Marlon <martianworm17@gmail.com>
2018-03-28 19:03:41 +0800

    Add Lambda equivalent

--- src/com/naturalprogrammer/java8tutorial/App2.java
+++ src/com/naturalprogrammer/java8tutorial/App2.java
@@ -3,7 +3,10 @@ package com.naturalprogrammer.java8tutorial;
 public class App2 {
 public static void main(String[] args) {
 		
-		System.out.println(process("Hello World!", String::toUpperCase));
+		System.out.println(process("Hello World!", str -> str.toLowerCase()));
+		System.out.println(process("Hello World!", String::toLowerCase));
+		
+		
 	}
 
 	private static String process(String string, Processor processor) {
