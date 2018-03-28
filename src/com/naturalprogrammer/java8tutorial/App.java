3257f9967b3e50e120bd260033263ab58f440bc5
Marlon <martianworm17@gmail.com>
2018-03-28 17:00:11 +0800

    Passing Behavior

---
+++ src/com/naturalprogrammer/java8tutorial/App.java
@@ -0,0 +1,15 @@
+package com.naturalprogrammer.java8tutorial;
+
+/**
+ * 
+ * @author Lon
+ *
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+
+}
